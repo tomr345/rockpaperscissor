b7316e0b0bb9a138e03b166106c4e81a3483e263
Tom Rintoul <thomas.rintoul@icloud.com>
2022-04-27 16:04:26 +0100

    added buttons, labels, and all game logic except for updating the score labels

--- javascript.js
+++ javascript.js
@@ -14,49 +14,86 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'Rock' && computerSelection == 'Paper') {
-        return "You Lose! Paper beats Rock";
+        // return "You Lose! Paper beats Rock";
+        setResults("You Lose! Paper beats Rock", "red");
         computerScore++;
     }
     else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
-        return "You Win! Rock beats Scissors!";
+        // return "You Win! Rock beats Scissors!";
+        setResults("You Win! Rock beats Scissors!", "green");
         userScore++;
     }
     
     else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
-        return "You Win! Scissors beats Paper!";
+        // return "You Win! Scissors beats Paper!";
+        setResults("You Win! Scissors beats Paper!", "green");
         userScore++;
     }
 
     else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
-        return "You Lose! Rock beats Scissors!";
+        // return "You Lose! Rock beats Scissors!";
+        setResults("You Lose! Rock beats Scissors!", "red");
         computerScore++;
     }
 
     else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
-        return "You Lose! Scissors beats Paper!";
+        // return "You Lose! Scissors beats Paper!";
+        setResults("You Lose! Scissors beats Paper!", "red");
         computerScore++;
     }
 
     else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
-        return "You Win! Paper beats Rock!";
+        // return "You Win! Paper beats Rock!";
+        setResults("You Win! Paper beats Rock!", "green");
         userScore++;
     }
 
     else {
-        return "It's a draw!"
-        userScore++;
-        computerScore++;
+        // return "It's a draw!"
+        setResults("It's a draw!", "orange");
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const userInput = prompt('Choose Rock, Paper or Scissors: ');
-        console.log(playRound(userInput, computerPlay()));
-        console
+function setResults(entry, colour) {
+    const resultString = entry;
+    const newColour = colour;
+
+    const p = document.getElementById('results');
+    p.textContent = resultString;
+    p.style.color = newColour;    
+}
+
+function updateScore(winner) {
+    const player = winner;
+    const userLabel = document.getElementById('userLabel');
+    const computerLabel = document.getElementById('computerLabel');
+    
+    if (winner == "user") {
+        userScore++;
+        userLabel.textContent = userScore;
     }
-    userScore = 0;
-    computerScore = 0;
+    else {
+        computerScore++;
+        computerLabel.textContent = computerScore;
+    }
+} 
+
+function game() {
+    const buttons = document.querySelectorAll('button');
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', function (e) {
+            playRound(e.target.id, computerPlay());
+        })
+    })
+
+    
+    // for (let i = 0; i < 5; i++) {
+    //     // const userInput = prompt('Choose Rock, Paper or Scissors: ');
+    //     console.log(playRound(userInput, computerPlay()));
+    // }
+    // userScore = 0;
+    // computerScore = 0;
 }
 
 game();
